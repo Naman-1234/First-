270a8596531a997a85579205eb3f0f43b0de422b
Naman-1234 <63748249+Naman-1234@users.noreply.github.com>
2020-05-20 17:14:54 +0530

    Create Finding-Duplicates-in-python.py

---
+++ Finding-Duplicates-in-python.py
@@ -0,0 +1,12 @@
+#First we will write a function to check whether it contains dupliacte or not For this i will remember that a set is a data structure in which duplicates are not allowed.
+def check(list):
+  if len(list)==len(set(list)):
+    return 0
+  else:
+    return 1
+    
+    
+    
+    
+  
+
