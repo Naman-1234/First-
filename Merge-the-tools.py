4f048d5e4d7254862da878936f96123c8cd5e1b4
Naman-1234 <63748249+Naman-1234@users.noreply.github.com>
2020-05-20 17:55:04 +0530

    Create Merge-the-tools.py

---
+++ Merge-the-tools.py
@@ -0,0 +1,17 @@
+from collections import OrderedDict         
+def merge_the_tools(string, k):
+    x=""
+    for i in range(0,len(string),k):
+        x+=string[i:i+k]
+        x+='- '
+    l=list()
+    l=x.split('-')
+    for i in l:
+        i="".join((OrderedDict.fromkeys(i)))
+    x=" ".join(l)
+    l=x.split()
+    for i in l:
+        print(i)
+
+            
+if __name__ == '__main__':
