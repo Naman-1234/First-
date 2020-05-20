af2a44c436e9b6a2a856e1a655910d7a0d6d2664
Naman-1234 <63748249+Naman-1234@users.noreply.github.com>
2020-05-20 17:46:40 +0530

    Create Use-of-Libraries.py

---
+++ Use-of-Libraries.py
@@ -0,0 +1,19 @@
+#import turtle
+#turtle.forward(101)
+#turtle.right(1000)
+#turtle.circle(19)
+#turtle.done()
+#import webbrowser
+#c = webbrowser.get('Firefox')
+#c.open('http://www.python.org')
+import time
+from time import time as my_timer
+import random
+input("Enter")
+#wait=random.randint(1,6)
+#time.sleep(wait)
+start=my_timer()
+input("stop")
+end=my_timer()
+print("Your elapsed time is {}".format(end-start))
+print("time is {}".format(time.gmtime()))
